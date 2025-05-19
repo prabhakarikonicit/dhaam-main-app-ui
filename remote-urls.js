aa17271603b23cb78e70a5d8e6eef7f132428e74
prabhakarikonicit <prabhakar.koppireddy@ikonicit.com>
2025-05-19 23:51:00 +0530

    dynamic env changes
    dynamic env changes

---
+++ remote-urls.js
@@ -0,0 +1,30 @@
+const env = process.env.APP_ENV || "local";
+
+const urls = {
+  local: {
+    dhaam_order_app_ui: "dhaam_order_app_ui@http://localhost:3002/dhaam_order_app_ui.js",
+    dhaam_settings_app_ui: "dhaam_settings_app_ui@http://localhost:3003/dhaam_settings_app_ui.js",
+    dhaam_store_app_ui: "dhaam_store_app_ui@http://localhost:3004/dhaam_store_app_ui.js",
+    dhaam_menu_app_ui: "dhaam_menu_app_ui@http://localhost:3006/dhaam_menu_app_ui.js",
+  },
+  dev: {
+    dhaam_order_app_ui: "dhaam_order_app_ui@http://dhaam-react.s3-website-us-west-2.amazonaws.com/dhaam-order/remoteEntry.js",
+    dhaam_settings_app_ui: "dhaam_settings_app_ui@http://dhaam-react.s3-website-us-west-2.amazonaws.com/dhaam-settings/remoteEntry.js",
+    dhaam_store_app_ui: "dhaam_store_app_ui@http://dhaam-react.s3-website-us-west-2.amazonaws.com/dhaam-store/remoteEntry.js",
+    dhaam_menu_app_ui: "dhaam_menu_app_ui@http://dhaam-react.s3-website-us-west-2.amazonaws.com/dhaam-menu/remoteEntry.js",
+  },
+  test: {
+    // ...add test URLs
+  },
+  stage: {
+    // ...add stage URLs
+  },
+  prod: {
+    dhaam_order_app_ui: "dhaam_order_app_ui@https://dev.example.com/order/remoteEntry.js",
+    dhaam_settings_app_ui: "dhaam_settings_app_ui@https://dev.example.com/settings/remoteEntry.js",
+    dhaam_store_app_ui: "dhaam_store_app_ui@https://dev.example.com/store/remoteEntry.js",
+    dhaam_menu_app_ui: "dhaam_menu_app_ui@https://dev.example.com/menu/remoteEntry.js",
+  },
+};
+
+export default urls[env];
