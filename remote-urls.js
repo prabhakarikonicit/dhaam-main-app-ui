83f2f3006d3bc50558b1b3bf80aabeb6a34f3d6f
prabhakarikonicit <prabhakar.koppireddy@ikonicit.com>
2025-05-21 14:36:40 +0530

    env chagnes
    env chagnes

--- remote-urls.js
+++ remote-urls.js
@@ -9,10 +9,10 @@ const urls = {
     dhaam_menu_app_ui: "dhaam_menu_app_ui@http://localhost:3006/dhaam_menu_app_ui.js",
   },
   dev: {
-    dhaam_order_app_ui: "dhaam_order_app_ui@http://dhaam-react.s3-website-us-west-2.amazonaws.com/dhaam-order/remoteEntry.js",
-    dhaam_settings_app_ui: "dhaam_settings_app_ui@http://dhaam-react.s3-website-us-west-2.amazonaws.com/dhaam-settings/remoteEntry.js",
-    dhaam_store_app_ui: "dhaam_store_app_ui@http://dhaam-react.s3-website-us-west-2.amazonaws.com/dhaam-store/remoteEntry.js",
-    dhaam_menu_app_ui: "dhaam_menu_app_ui@http://dhaam-react.s3-website-us-west-2.amazonaws.com/dhaam-menu/remoteEntry.js",
+    dhaam_order_app_ui: "dhaam_order_app_ui@http://dhaam-react.s3-website-us-west-2.amazonaws.com/dhaam-order/dhaam_order_app_ui.js",
+    dhaam_settings_app_ui: "dhaam_settings_app_ui@http://dhaam-react.s3-website-us-west-2.amazonaws.com/dhaam-settings/dhaam_settings_app_ui.js",
+    dhaam_store_app_ui: "dhaam_store_app_ui@http://http://dhaam-react.s3-website-us-west-2.amazonaws.com/dhaam-store/dhaam_store_app_ui.js",
+    dhaam_menu_app_ui: "dhaam_menu_app_ui@http://http://dhaam-react.s3-website-us-west-2.amazonaws.com/dhaam-menu/dhaam_menu_app_ui.js",
   },
   test: {
     // ...add test URLs
@@ -21,11 +21,10 @@ const urls = {
     // ...add stage URLs
   },
   prod: {
-    
-    dhaam_order_app_ui: "dhaam_order_app_ui@http://dhaam-react.s3-website-us-west-2.amazonaws.com/dhaam-order/remoteEntry.js",
-    dhaam_settings_app_ui: "dhaam_settings_app_ui@http://dhaam-react.s3-website-us-west-2.amazonaws.com/dhaam-settings/remoteEntry.js",
-    dhaam_store_app_ui: "dhaam_store_app_ui@http://dhaam-react.s3-website-us-west-2.amazonaws.com/dhaam-store/remoteEntry.js",
-    dhaam_menu_app_ui: "dhaam_menu_app_ui@http://dhaam-react.s3-website-us-west-2.amazonaws.com/dhaam-menu/remoteEntry.js",
+    dhaam_order_app_ui: "dhaam_order_app_ui@http://dhaam-react.s3-website-us-west-2.amazonaws.com/dhaam-order/dhaam_order_app_ui.js",
+    dhaam_settings_app_ui: "dhaam_settings_app_ui@http://dhaam-react.s3-website-us-west-2.amazonaws.com/dhaam-settings/dhaam_settings_app_ui.js",
+    dhaam_store_app_ui: "dhaam_store_app_ui@http://http://dhaam-react.s3-website-us-west-2.amazonaws.com/dhaam-store/dhaam_store_app_ui.js",
+    dhaam_menu_app_ui: "dhaam_menu_app_ui@http://http://dhaam-react.s3-website-us-west-2.amazonaws.com/dhaam-menu/dhaam_menu_app_ui.js",
   },
 };
 
