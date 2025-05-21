b6310e60870b03cbd74b30a09a678c045e6825cd
prabhakarikonicit <109589313+prabhakarikonicit@users.noreply.github.com>
2025-05-21 20:14:35 +0530

    Update remote-urls.js

--- remote-urls.js
+++ remote-urls.js
@@ -11,8 +11,8 @@ const urls = {
   dev: {
     dhaam_order_app_ui: "dhaam_order_app_ui@http://dhaam-react.s3-website-us-west-2.amazonaws.com/dhaam-order/dhaam_order_app_ui.js",
     dhaam_settings_app_ui: "dhaam_settings_app_ui@http://dhaam-react.s3-website-us-west-2.amazonaws.com/dhaam-settings/dhaam_settings_app_ui.js",
-    dhaam_store_app_ui: "dhaam_store_app_ui@http://http://dhaam-react.s3-website-us-west-2.amazonaws.com/dhaam-store/dhaam_store_app_ui.js",
-    dhaam_menu_app_ui: "dhaam_menu_app_ui@http://http://dhaam-react.s3-website-us-west-2.amazonaws.com/dhaam-menu/dhaam_menu_app_ui.js",
+    dhaam_store_app_ui: "dhaam_store_app_ui@http://dhaam-react.s3-website-us-west-2.amazonaws.com/dhaam-store/dhaam_store_app_ui.js",
+    dhaam_menu_app_ui: "dhaam_menu_app_ui@http://dhaam-react.s3-website-us-west-2.amazonaws.com/dhaam-menu/dhaam_menu_app_ui.js",
   },
   test: {
     // ...add test URLs
